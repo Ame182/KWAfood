0ad72965c856efce3b445ead04fa3f7d1c823a2a
amelia <amelita182@gmail.com>
2019-04-03 16:32:09 -0300

    listo jquery

--- HTML base KWAfood/app.js
+++ HTML base KWAfood/app.js
@@ -1,25 +1,68 @@
+$(document).ready(function() {
+    $(".dia.l").hover( function(e) {
+		e.preventDefault()
+    	$('.hide.lun').toggle('hide')  
+    });
+
+
+    $(".dia.m").hover( function(e) {
+		e.preventDefault()
+    	$('.hide.mar').toggle('hide')
+
+    });
+
+    $(".dia.mi").hover( function(e) {
+		e.preventDefault()
+    	$('.hide.mie').toggle('hide')
+
+    });
+
+    $(".dia.ju").hover( function(e) {
+		e.preventDefault()
+    	$('.hide.jue').toggle('hide')
+
+    });
+
+    $(".dia.vi").hover( function(e) {
+		e.preventDefault()
+    	$('.hide.vie').toggle('hide')
+
+    });
+
+    $(".dia.sado").hover( function(e) {
+		e.preventDefault()
+    	$('.hide.sa').toggle('hide')
+
+    });
+    
+
+
+//GALERIA
+
 $(function(){
   $('.gall').click(function(){
       $('.satura').not(this).removeClass('satura');
       $(this).toggleClass('satura');
       
 
-  })
-})
-
-$('.mensaje-diario').dailyMessages({
-    monday: "Hoy es Lunes dia del Arepa!",
-    tuesday: "Hoy es Martes, dia del Taco! ",
-    wednesday: "Hoy es Miercoles dia del Ceviche!",
-    thursday: "Hoy es Jueves dia del Asado!",
-    friday: "Hoy es Viernes pasa un genial fin de semana!",
-    saturday: "Hoy es Sabado como lo estas pasado?",
-    sunday: "Ho es Domingo y celebramos el dia de la Madre",
-    success: function() {
-        console.log('Funciona!');
-    },
-    error: function(message) {
-        console.log(message);
-        $(this).remove();
-    }
+  });
 });
+
+
+
+//NOTICIAS
+
+$(function () {
+  $(".leerMas").bind("click", function () { });
+});
+
+$(function () {
+  $(".leerMas").bind("click", function () {
+    $(this).text($(this).text() == "Leer más" ? "Ocultar" : "Leer más");
+    $(this).prev().slideToggle();
+  });
+});
+
+
+});
+
