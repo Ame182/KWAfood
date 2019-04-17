1e7e4d5c192dadb12e5e426eb0c66066a646f9d3
amelia <amelita182@gmail.com>
2019-04-16 23:09:50 -0400

    agregando presentacion

--- KWAfood/wp-content/themes/KWAfood/page-home.php
+++ KWAfood/wp-content/themes/KWAfood/page-home.php
@@ -5,25 +5,26 @@
 	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/LOGO.svg" class="animated bounce delay-2s" id="logoAbsoluto" alt="Logo KWA food">
 	  <div class="carousel-inner">
 
-	    <div class="carousel-item active">
-	      <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/slide1a.jpg" alt="First slide">
+	    <div class="carousel-item active" data-interval="2000">
+	      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/slide1a.jpg" class="d-block w-100"  alt="First slide">
 	    </div>
-	    <div class="carousel-item">
-	      <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/slide2a.jpg" alt="Second slide">
+	    <div class="carousel-item" data-interval="2000">
+	      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/slide2a.jpg" class="d-block w-100"  alt="Second slide">
 	    </div>
-	    <div class="carousel-item">
-	      <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/assets/images/slide3a.jpg" alt="Third slide">
+	    <div class="carousel-item" data-interval="2000">
+	      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/slide3a.jpg"  class="d-block w-100" alt="Third slide">
 	    </div>
 	  </div>
-	  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+	  <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
 	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
 	    <span class="sr-only">Previous</span>
 	  </a>
-	  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+	  <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
 	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
 	    <span class="sr-only">Next</span>
 	  </a>
 </div>
+
 <!-- Termina el carrusel -->
 
 </header>
